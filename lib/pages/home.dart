271597c2301d509fa65510c9a9cf9a5d8cd5327f
Akash <mahmudakash177@gmail.com>
2023-10-25 13:15:14 +0600

    category section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,39 +1,111 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
       appBar: appbar(),
       backgroundColor: Colors.white,
-      body:  Column(children: [
-        _searchField()
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        _searchField(),
+        SizedBox(
+          height: 40,
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text("Category",
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600)),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            _categoriesSection()
+          ],
+        )
       ]),
     );
   }
 
+  Container _categoriesSection() {
+    return Container(
+            height: 120,
+            child: ListView.separated(
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.only(left: 20, right: 20),
+              separatorBuilder: (context, index)=>SizedBox(width: 25,),
+              itemBuilder: (contex, index) {
+                return Container(
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: categories[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(16)
+                  ),
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle
+                        ),
+                        padding: EdgeInsets.all(8),
+                        child: SvgPicture.asset(categories[index].iconPath),
+                      ),
+                      Text(
+                        categories[index].name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+  }
+
   Container _searchField() {
     return Container(
-        margin: const EdgeInsets.only(top: 40, left: 20, right: 20) ,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-color:  Color(0xff1d1617).withOpacity(0.11),
-blurRadius: 40,
-spreadRadius: 0.0
-          )]
-        ),
-        child:  TextField(
-          decoration: InputDecoration(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            color: Color(0xff1d1617).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0)
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
             filled: true,
             hintText: "Search for raining",
-            hintStyle: TextStyle(
-              color: Color(0xffDDDADA),
-              fontSize: 14
-            ),
+            hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
             prefixIcon: Padding(
               padding: const EdgeInsets.all(12),
               child: SvgPicture.asset('assets/icons/Search.svg'),
@@ -45,10 +117,10 @@ spreadRadius: 0.0
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     VerticalDivider(
-              thickness: 0.1,
-              color: Colors.black,
-              indent: 10,
-              endIndent: 10,
+                      thickness: 0.1,
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
                     ),
                     Padding(
                       padding: const EdgeInsets.all(12),
@@ -62,11 +134,9 @@ spreadRadius: 0.0
             contentPadding: EdgeInsets.all(15),
             border: OutlineInputBorder(
               borderSide: BorderSide.none,
-
-            )
-          ),
-        ),
-      );
+            )),
+      ),
+    );
   }
 
   AppBar appbar() {
@@ -80,9 +150,7 @@ spreadRadius: 0.0
       backgroundColor: Colors.white,
       elevation: 0,
       leading: GestureDetector(
-        onTap: () {
-          
-        },
+        onTap: () {},
         child: Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
